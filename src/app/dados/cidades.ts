aa1831c2b2cd1509c7039dcfca359342b65ceaad
giovanimohrboso <giovani.mohr@gmail.com>
2024-01-19 16:09:31 -0300

    Componente destaque

--- src/app/dados/cidades.ts
+++ src/app/dados/cidades.ts
@@ -1,7 +1,7 @@
 export const cidades = [
 {"id":"1",
-  "nome":"Tiete-SP",
-  "descricao":"cidade dos jardins",
+  "nome":"Águas de São Pedro - SP",
+  "descricao":"A estância de Águas de São Pedro foi fundada em 25 de julho de 1934 por Octavio Moura Andrade e virou município em 24 de dezembro de 1948, emancipando-se de São Pedro. Sua instalação oficial ocorreu 2 de abril de 1949. Seu aniversário é comemorado na data de fundação.Com apenas 3,64 km² de área e uma população de 3.451 habitantes (2023), Águas de São Pedro é considerada a menor cidade do estado de São Paulo e segunda menor do Brasil.Descoberta das Águas: Na década de 1920, Júlio Prestes, então governador do estado de São Paulo, havia iniciado as pesquisas na área de prospecção de petróleo. As pesquisas falharam na tentativa de encontrar o produto e os equipamentos foram abandonados, apenas jorrando água mineral. Posteriormente outras tentativas foram feitas para encontrar petróleo em grandes profundidades e, novamente, nenhum óleo foi encontrado. Apenas água.Anos mais tarde, em 1934, Ângelo Franzin, donos das terras perfuradas, que é conhecida atualmente como fonte da juventude, construiu um balneário simples, onde se banhava. A água tinha um odor forte. Um ano depois um grupo de pessoas da cidade comprou e construiu um balneário ao redor da fonte da juventude.Águas de São Pedro tem opções deliciosas para passeio – nessa viagem, passamos um dia nessa linda e aconchegante cidade.",
   "caminhoFotoCapa":"assets/cidades/aguasdesaopedro-sp/fotocapa.png",
   "destaque":true
 },
